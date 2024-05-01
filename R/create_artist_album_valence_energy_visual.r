9d9fecc168880119a4ccee7ed8452d9edae8c13e
AdamDelRio <122654004+AdamDelRio@users.noreply.github.com>
2024-05-01 11:54:39 -0700

    Checking font change

--- R/create_artist_album_valence_energy_visual.r
+++ R/create_artist_album_valence_energy_visual.r
@@ -97,7 +97,8 @@ create_artist_album_valence_energy_visual <- function(query = NULL, artist_id = 
       legend.text = ggplot2::element_text(size = 10),
       legend.position = "bottom",
       legend.direction = "vertical",
-      plot.margin = ggplot2::margin(t = 50)
+      plot.margin = ggplot2::margin(t = 50),
+      legend.text = ggplot2::element_text(size = 15)
     ) +
     ggplot2::ggtitle(paste("Album Valence-Energy Visualization\nfor", artist_name)) +
     ggplot2::theme(plot.title.position = "plot"))
